65516b7201010a13ac8e593c0d0d4ab58cf27685
Robin Jaiswal <jaiswalji903@gmail.com>
2026-05-08 22:22:44 +0530

    Functions and Recursion

---
+++ chapter8  functions and recursions/practice set/08problem.py
@@ -0,0 +1,5 @@
+# write a python function to print multiplication table of a given number?
+def multiply(n):
+    for i in range(1, 11):
+        print(f"{n}*{i}={n*i}")
+multiply(6)
---
+++ chapter8  functions and recursions/returb.py
@@ -0,0 +1,6 @@
+def goodDay(name , ending):
+    print("Good Day, " + name)
+    print(ending)
+    return "done"
+a=goodDay("RObin", "Thank you")
+print(a)
